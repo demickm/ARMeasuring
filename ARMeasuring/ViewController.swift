ba49a6e02c123e6bdc95cdfff5ff04602bfca92f
demickm <demickm@hotmail.com>
2017-12-07 17:01:55 -0700

    Setup of ARMeasuring

--- ARMeasuring/ViewController.swift
+++ ARMeasuring/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import ARKit
+class ViewController: UIViewController, ARSCNViewDelegate {
 
-class ViewController: UIViewController {
-
+    // Outlets
+    
+    @IBOutlet weak var zLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    // Variables and Constants
+    
+    let configuration = ARWorldTrackingConfiguration()
+    var startingPosition: SCNNode?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Basic Setup
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        sceneView.session.run(configuration)
+        sceneView.autoenablesDefaultLighting = true
+        
+        // Tap Gesture Setup
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+        self.sceneView.delegate = self
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +43,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        guard let sceneView = sender.view as? ARSCNView,
+              let currentFrame = sceneView.session.currentFrame else {return}
+        let camera = currentFrame.camera
+        let transform = camera.transform
+        var translationMatrix = matrix_identity_float4x4
+        translationMatrix.columns.3.z = -0.1
+        let modifiedMatrix = simd_mul(transform, translationMatrix)
+        let sphere = SCNNode(geometry: SCNSphere(radius: 0.005))
+        sphere.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        sphere.simdTransform = modifiedMatrix
+        sceneView.scene.rootNode.addChildNode(sphere)
+        startingPosition = sphere
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        guard let startingPosition = self.startingPosition,
+              let pointOfView = self.sceneView.pointOfView else { return }
+        let transform = pointOfView.transform
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let xDistance = location.x - startingPosition.position.x
+        let yDistance = location.y - startingPosition.position.y
+        let zDistance = location.z - startingPosition.position.z
+        DispatchQueue.main.async {
+            self.xLabel.text = "X: " + String(format: "%.2f", xDistance) + "m"
+            self.yLabel.text = "Y: " + String(format: "%.2f", yDistance) + "m"
+            self.zLabel.text = "Z: " + String(format: "%.2f", zDistance) + "m"
+        }
+    }
 
 }
 
